0b5f167c729ad4bdd9039039a263f383e40a446d
Muhammad Ahmed <mahmedsalman2000@gmail.com>
2022-12-16 18:34:12 +0500

    New things.

--- 1st-lecture.js
+++ 1st-lecture.js
@@ -5,3 +5,12 @@ b.addEventListener('click', showMsg);
 function showMsg() {
     alert("Hello I am Watching you,I know where you live! :)");
 }
+
+let bb = document.getElementById('me');
+
+bb.addEventListener('click', inputMsg);
+
+function inputMsg() {
+    let name = prompt();
+    alert("My name is " + name);
+}
