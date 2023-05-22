23ab9226fcf00161e4999a570a13d70b1efc5e83
Muhammad Ahmed <mahmedsalman2000@gmail.com>
2023-05-22 17:01:31 +0500

    Practicing JavaScript.

---
+++ practice.js
@@ -0,0 +1,18 @@
+var age = 15;
+const names = 'Hamid & Ali';
+console.log(names);
+
+let laptop = 'data';
+laptop = 'games';
+console.log(laptop);
+
+let ShirtsPrices = [6447, 7335, 2540, 2345, 5673];
+console.log('ShirtsPrices are: ' + ShirtsPrices);
+
+let ShirtsNames = ['Gray Shirt', 'Red&Yellow Shirt', 'LightBlue Shirt', 'Green Shirt', 'Brown Shirt'];
+console.log('ShirtsNames are: ' + ShirtsNames);
+
+let Shirts = { 'Gray Shirt': 6447, 'Red&Yellow Shirt': 7335, 'LightBlue Shirt': 2540, 'Green Shirt': 2345, 'Brown Shirt': 5673 };
+
+var key = 'Red&Yellow Shirt';
+console.log('Price of ' + key + ' is: ' + Shirts[key]);
